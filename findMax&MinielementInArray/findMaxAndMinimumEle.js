8909ff38d969df8fa6f0e2992f5417309ef359dd
adithanbs <adithansb@gmail.com>
2023-05-11 07:36:06 +0530

    day-4

---
+++ findMax&MinielementInArray/findMaxAndMinimumEle.js
@@ -0,0 +1,20 @@
+/* let arr = [1,3,5,7,9]
+function plusMinus(arr) {
+    // Write your code here
+    let max = arr[0]
+    let min = arr[0]
+    for(let i of arr){
+if(i > max){
+max = i
+}
+if(i<max) {
+min = i
+}       }
+    }
+
+
+// console.log(negative);
+// console.log(zero);
+ 
+}
+plusMinus(arr); */
---
+++ matrix/sumMatrixdiagnals.js
@@ -0,0 +1,17 @@
+let a1 = [1,2,3];
+let a2 = [4,5,6];
+let a3 = [7,8,9];
+
+let matrix = [a1,a2,a3]
+let m1 = 0;  
+let m2 = 0;
+for(let i = 0; i < matrix.length; i++){
+/* m1 += matrix[i][i] */
+/* m2 += martrix[i].length[-i]; */
+/* console.log(matrix[i][matrix[i].length-i]) */
+/* console.log(i) */
+ console.log(matrix[matrix[i].length-i]); 
+}
+
+/* console.log(m1)
+console.log(m2) */
